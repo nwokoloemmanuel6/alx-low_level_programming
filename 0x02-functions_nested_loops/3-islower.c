224ee0166edcef5a8730eefc2bd9ebf6434adac5
nwokolo chukwuebuka emmanuel <nwokoloebuka84@gmail.com>
2022-03-17 00:07:15 +0100

    my commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _islower - return 0 letter not lowercase
+ * 1 letter lowercase
+ * baccrie © 2022 all right reserved
+ * @c: The int to print
+ * Return: 0 not lowercase, 1 lowercase
+ */
+int _islower(int c)
+
+{
+
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
