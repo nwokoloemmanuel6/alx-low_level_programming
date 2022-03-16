daf5cd75cb7d56a38e85ebda0e5a44fc63131c2b
nwokolo chukwuebuka emmanuel <nwokoloebuka84@gmail.com>
2022-03-16 23:05:41 +0100

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * The main.h is an header file that has th
+ * the prototyping definition of the
+ * _putchar function
+ * copyright © baccrie 2022
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
